6770f3c2f7232f706c550eb8acad521537d9fb16
yutendo <mayuyuto0714@gmail.com>
2025-06-29 23:39:31 +0900

    make FormRequestValidation

---
+++ src/app/Http/Requests/FibonacciRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+
+class FibonacciRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'n' => ['required', 'integer', 'min:0', 'max:1000000'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new ValidationException($validator);
+    }
+}
