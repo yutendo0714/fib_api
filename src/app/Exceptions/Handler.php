3d3481498b6625b854ab3e38c98d7fb6c81bc87c
yutendo <mayuyuto0714@gmail.com>
2025-06-29 23:38:28 +0900

    make ValidationErrorHandler

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +26,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (ValidationException $e) {
+            return response()->json([
+                'status' => 400,
+                'message' => 'Bad request.',
+                // 'errors' => $e->errors(),
+            ], 400);
         });
     }
 }
